aa816f924092871cca354436debd729e4858d838
wambui-g <grayciekamau03@gmail.com>
2023-12-21 16:40:18 +0300

    updated admin views minus tasks

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.form import Select2Widget
+from wtforms import SelectField
 from app.config import Config
 from app.models import User, Department, Project, Task
 
@@ -18,9 +20,38 @@ migrate = Migrate(app, db)
 
 admin = Admin(app)
 
-admin.add_view(ModelView(User, db.session))
+class UserView(ModelView):
+    column_list = ('id', 'username', 'email', 'password', 'department', 'created_at')
+    column_lables = {'department': 'Department'}
+    form_columns = ('username', 'email', 'password', 'department_id')
+
+    form_overrides = {
+            'department': SelectField
+    }
+    form_args = {
+            'department': {
+                'widget':Select2Widget()
+            }
+    }
+
+class ProjectView(ModelView):
+    column_list = ('id', 'name', 'department')
+    column_labels = {'department': 'Department'}
+    form_columns = ('name', 'department_id')
+
+    # Customize the form to use Select2Widget for the department field
+    form_overrides = {
+        'department': SelectField
+    }
+    form_args = {
+        'department': {
+            'widget': Select2Widget()
+        }
+    }
+
+admin.add_view(UserView(User, db.session))
 admin.add_view(ModelView(Department, db.session))
-admin.add_view(ModelView(Project, db.session))
+admin.add_view(ProjectView(Project, db.session))
 admin.add_view(ModelView(Task, db.session))
 
 @app.route('/')
